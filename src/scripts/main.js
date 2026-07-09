97ebf50864b1cbca250ce6c744b3881f818b6ee0
Shakhruz <shaxruzsohibnazarov2@gmail.com>
2026-07-09 17:56:40 +0500

    big changes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,21 +1,21 @@
-    async function fetchProducts() {
-    const res = await fetch('https://dummyjson.com/products')
-    const data = await res.json()
-    if (!data?.products) return;
-    const mappedPr = data.products.map((item)=>{
-        const imgPr = (item?.images && item.images.length > 0)
-            ? `<img class='prImg' src='${item.images[0]}' alt='${item.title}' />` 
-            : 'Mahsulot rasmi mavjud emas';
+//     async function fetchProducts() {
+//     const res = await fetch('https://dummyjson.com/products')
+//     const data = await res.json()
+//     if (!data?.products) return;
+//     const mappedPr = data.products.map((item)=>{
+//         const imgPr = (item?.images && item.images.length > 0)
+//             ? `<img class='prImg' src='${item.images[0]}' alt='${item.title}' />` 
+//             : 'Mahsulot rasmi mavjud emas';
 
 
-      return `<div>
-            ${imgPr}
-      </div>`
-    }).join('')
+//       return `<div>
+//             ${imgPr}
+//       </div>`
+//     }).join('')
 
 
-        document.querySelector('.container').innerHTML = mappedPr
+//         document.querySelector('').innerHTML = mappedPr
     
-}
-fetchProducts()
+// }
+// fetchProducts()
 
